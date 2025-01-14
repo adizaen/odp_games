402fcceb48219b0c7a48383d55223432bfe0bfae
slsrzka <rizkasalsa1801@gmail.com>
2025-01-14 16:32:17 +0700

    nomor 2

--- games/b.py
+++ games/b.py
@@ -1,5 +1,6 @@
 
 def b():
+
     """
     Diberikan sebuah list angka, cari pasangan angka dengan selisih terkecil.
 
@@ -11,4 +12,19 @@ def b():
     """
 
     # write the code solution here
-    print("Mohon maaf, permainan B belum tersedia!")
+    n = input("Masukkan angka: ")
+
+    n_list = [int(i) for i in n.split()]
+    urutan=sorted(n_list)
+    data=[]
+    data_terkecil = None
+    selisih_min = float('inf')
+    for i in range(1,len(urutan)):
+        hasil=urutan[i]-urutan[i-1]
+        hasil=abs(hasil)
+        data.append(hasil)
+        if hasil<selisih_min:
+            selisih_min=hasil
+            data_terkecil=(urutan[i-1],urutan[i])
+    print(data_terkecil) 
+
